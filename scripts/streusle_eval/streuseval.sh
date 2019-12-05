769fad134ddc411c377d5de6a554c345515c1f56
Nelson Liu <nfliu@nelsonliu.me>
2019-12-05 08:47:10 -0800

    Save streusle tagging results to a file.

--- scripts/streusle_eval/streuseval.sh
+++ scripts/streusle_eval/streuseval.sh
@@ -5,7 +5,7 @@ REF="$1"
 shift
 grep -v '^#' $REF | cut -d"	" -f4 > ${REF%lex}.upos
 for PRED in "$@"; do
-    PREFIX="${PRED%jsonl}".
+    PREFIX="${PRED%jsonl}"
     python scripts/streusle_eval/streusle_set_lextag.py "$REF" "$PRED" > "$PREFIX"json || exit 1
     python -m json2conllulex "$PREFIX"json > "$PREFIX"conllulex || exit 1
     python -m conllulex2UDlextag "$PREFIX"conllulex > "$PREFIX"UDlextag || exit 1
@@ -14,5 +14,5 @@ for PRED in "$@"; do
     #rm -f "$PREFIX"json "$PREFIX"conllulex "$PREFIX"UDlextag
     grep -v '^#' "$PREFIX"conllulex | cut -d"	" -f4 > "$PREFIX"conllu.upos || continue
     paste "$PREFIX"conllu.upos ${REF%lex}.upos > "$PREFIX"conllu.upos.predicted_actual
-    python -m streuseval "$REF" "$PREFIX"autoid.json
+    python -m streuseval "$REF" "$PREFIX"autoid.json > "$PREFIX"streusle_results
 done
