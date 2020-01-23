356daa05e7d48b4426ab9fa30bb93a24dc82ca1f
Nelson Liu <nfliu@nelsonliu.me>
2020-01-23 15:54:56 -0800

    Save mweval and ssteval results to file.

--- scripts/evaluate_on_streusle2.sh
+++ scripts/evaluate_on_streusle2.sh
@@ -28,11 +28,17 @@ for modelpath in models/streusle2_*; do
     python2 scripts/streusle2.0_scripts/tags2sst.py models/${modelname}/${modelname}_dev_predictions.jsonl.tags > models/${modelname}/${modelname}_dev_predictions.jsonl.tags.sst
 
     # Run mweval
+    if [ -f models/${modelname}/${modelname}_dev_predictions.mweval_results ] ; then
+        rm models/${modelname}/${modelname}_dev_predictions.mweval_results
+    fi
     python2 scripts/streusle2.0_scripts/mweval.py \
         data/streusle2.0/streusle.tags.sst.dev \
-        models/${modelname}/${modelname}_dev_predictions.jsonl.tags.sst
-    # Run mweval
+        models/${modelname}/${modelname}_dev_predictions.jsonl.tags.sst > models/${modelname}/${modelname}_dev_predictions.mweval_results
+    # Run ssteval
+    if [ -f models/${modelname}/${modelname}dev_predictions.ssteval_results ] ; then
+        rm models/${modelname}/${modelname}dev_predictions.ssteval_results
+    fi
     python2 scripts/streusle2.0_scripts/ssteval.py \
         data/streusle2.0/streusle.tags.sst.dev \
-        models/${modelname}/${modelname}_dev_predictions.jsonl.tags.sst
+        models/${modelname}/${modelname}_dev_predictions.jsonl.tags.sst > models/${modelname}/${modelname}dev_predictions.ssteval_results
 done
