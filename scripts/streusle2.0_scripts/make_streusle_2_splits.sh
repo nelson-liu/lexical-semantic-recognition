25fc41f82c752cf5a2abd7142892ae47c0172598
Nelson Liu <nfliu@nelsonliu.me>
2020-01-23 15:37:59 -0800

    make streusle2 splits from data with lemmas

--- scripts/streusle2.0_scripts/make_streusle_2_splits.sh
+++ scripts/streusle2.0_scripts/make_streusle_2_splits.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 python scripts/streusle2.0_scripts/make_streusle2_splits.py \
-    --data-path data/streusle2.0/streusle.sst \
+    --data-path data/streusle2.0/streusle.tags.sst \
     --train-ids-path data/streusle2.0/train.sentids \
     --test-ids-path data/streusle2.0/test.sentids \
     --num-dev-sentences 500 \
-    --train-output-path data/streusle2.0/streusle.sst.train \
-    --test-output-path data/streusle2.0/streusle.sst.test \
-    --dev-output-path data/streusle2.0/streusle.sst.dev \
+    --train-output-path data/streusle2.0/streusle.tags.sst.train \
+    --test-output-path data/streusle2.0/streusle.tags.sst.test \
+    --dev-output-path data/streusle2.0/streusle.tags.sst.dev \
