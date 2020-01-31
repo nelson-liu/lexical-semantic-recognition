f655b8bca4d10fdbe53a2adcc8c49556cacf8125
Nelson Liu <nfliu@nelsonliu.me>
2020-01-31 13:27:51 -0800

    Add scripts to evaluate on streusle test set

--- scripts/evaluate_on_streusle.sh
+++ scripts/evaluate_on_streusle.sh
@@ -19,4 +19,17 @@ for modelpath in models/streusle_*; do
         data/streusle/streusle.ud_dev.conllulex \
         "models/${modelname}/${modelname}_dev_predictions.jsonl"
 
+    allennlp predict models/${modelname}/model.tar.gz data/streusle/streusle.ud_test.json \
+        --silent \
+        --output-file models/${modelname}/${modelname}_test_predictions.jsonl \
+        --silent \
+        --include-package streusle_tagger \
+        --use-dataset-reader \
+        --predictor streusle-tagger \
+        --cuda-testice 0 \
+        --batch-size 64
+
+    ./scripts/streusle_eval/streuseval.sh \
+        data/streusle/streusle.ud_test.conllulex \
+        "models/${modelname}/${modelname}_test_predictions.jsonl"
 done
