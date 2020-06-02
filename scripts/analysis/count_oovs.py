2c69f533136c04cc28fd91de5e3e8e271f0a4a6d
Nelson Liu <nfliu@nelsonliu.me>
2020-06-01 17:23:50 -0700

    Print OOVs per lexcat for streusle

--- scripts/analysis/count_oovs.py
+++ scripts/analysis/count_oovs.py
@@ -1,7 +1,6 @@
 import argparse
-from collections import Counter
+from collections import defaultdict, Counter
 import json
-import re
 
 from tqdm import tqdm
 
@@ -24,29 +23,43 @@ def main(train_path, test_path):
     with open(test_path, "r") as test_file:
         test_data = json.load(test_file)
     test_tags = []
+    test_lexcats = []
     test_words = []
     for instance in tqdm(test_data):
         labels = [x["lextag"] for x in instance["toks"]]
+        lexcats = []
+        for label in labels:
+            if len(label.split("-")) > 1:
+                lexcats.append(label.split("-")[1])
+            else:
+                lexcats.append(None)
         tokens = [x["word"] for x in instance["toks"]]
         test_tags.append(labels)
+        test_lexcats.append(lexcats)
         test_words.append(tokens)
 
     # Count the number of OOV tokens
+    lexcat_oovs = defaultdict(Counter)
     num_test_tokens = 0
     num_oov_test_tokens = 0
     num_oov_and_mwe_test_tokens = 0
-    for test_example_words, test_example_tags in zip(test_words, test_tags):
-        for test_word, test_tag in zip(test_example_words, test_example_tags):
+    for test_example_words, test_example_lexcats, test_example_tags in zip(test_words, test_lexcats, test_tags):
+        for test_word, test_lexcat, test_tag in zip(test_example_words, test_example_lexcats, test_example_tags):
             if test_word not in train_vocab:
                 num_oov_test_tokens += 1
+                lexcat_oovs[test_lexcat]["oov"] += 1
                 if test_tag.split("-")[0].lower() != "o":
                     num_oov_and_mwe_test_tokens += 1
             num_test_tokens += 1
+            lexcat_oovs[test_lexcat]["total"] += 1
     print(f"Number of test tokens: {num_test_tokens}")
     print(f"OOV test tokens: {num_oov_test_tokens / num_test_tokens} "
           f"({num_oov_test_tokens} / {num_test_tokens})")
     print(f"OOV + MWE test tokens: {num_oov_and_mwe_test_tokens / num_test_tokens} "
           f"({num_oov_and_mwe_test_tokens} / {num_test_tokens})")
+    print("OOV frequency for each lexcat:")
+    for lexcat in lexcat_oovs:
+        print(f"{lexcat}: {100 * lexcat_oovs[lexcat]['oov']/lexcat_oovs[lexcat]['total']:.1f}")
 
 
 if __name__ == "__main__":
